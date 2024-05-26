36c956efe43fe2accfe8654e29310b00c539bdce
giu17 <giu17@MYASUS>
2024-05-26 18:28:05 +0100

    Create all components

---
+++ src/app/mobile/mobile-casa-di-cofano/mobile-casa-di-cofano.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-mobile-casa-di-cofano',
+  standalone: true,
+  imports: [],
+  templateUrl: './mobile-casa-di-cofano.component.html',
+  styleUrl: './mobile-casa-di-cofano.component.scss'
+})
+export class MobileCasaDiCofanoComponent {
+
+}
---
+++ src/app/mobile/mobile-casa-di-vito/mobile-casa-di-vito.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-mobile-casa-di-vito',
+  standalone: true,
+  imports: [],
+  templateUrl: './mobile-casa-di-vito.component.html',
+  styleUrl: './mobile-casa-di-vito.component.scss'
+})
+export class MobileCasaDiVitoComponent {
+
+}
