fdb75f4676ff48e4a70dcd250cb14ae496657d14
giu17 <giu17@MYASUS>
2024-05-26 23:39:41 +0100

    mobile casa di Vito

--- src/app/mobile/mobile-casa-di-vito/mobile-casa-di-vito.component.ts
+++ src/app/mobile/mobile-casa-di-vito/mobile-casa-di-vito.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-mobile-casa-di-vito',
@@ -8,5 +9,11 @@ import { Component } from '@angular/core';
   styleUrl: './mobile-casa-di-vito.component.scss'
 })
 export class MobileCasaDiVitoComponent {
+
+  constructor(private router: Router) {}
+
+  redirectTo(url: string) {
+    window.location.href = url
+  }
 
 }
