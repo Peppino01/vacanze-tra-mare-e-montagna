eb1694aec7312d2c5e8fa58223fd8179e5474427
giu17 <giu17@MYASUS>
2024-06-01 14:24:20 +0100

    Implement carousel

--- src/app/desktop/desktop-casa-di-vito/desktop-casa-di-vito.component.ts
+++ src/app/desktop/desktop-casa-di-vito/desktop-casa-di-vito.component.ts
@@ -1,14 +1,35 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, ViewChild } from '@angular/core';
+import { NgbCarousel, NgbCarouselModule } from '@ng-bootstrap/ng-bootstrap';
 import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-desktop-casa-di-vito',
   standalone: true,
-  imports: [ TranslateModule ],
+  imports: [ CommonModule, TranslateModule, NgbCarouselModule ],
   templateUrl: './desktop-casa-di-vito.component.html',
   styleUrl: './desktop-casa-di-vito.component.scss'
 })
 export class DesktopCasaDiVitoComponent {
+
+  @ViewChild('carousel', { static: true }) carousel!: NgbCarousel;
+
+  images = [1, 2, 3, 4].map((n) => `assets/pictures/desktop/la casa di Vito/${n}.jpg`);
+  isFullScreen: boolean = false;
+  
+  constructor() {}
+
+  toggleFullScreen() {
+    this.isFullScreen = !this.isFullScreen;
+    if (this.isFullScreen)
+    {
+      this.carousel.pause();
+    }
+    else
+    {
+      this.carousel.cycle();
+    }
+  }
 
   redirectTo(url: string) {
     window.location.href = url
--- src/app/mobile/mobile-casa-di-vito/mobile-casa-di-vito.component.ts
+++ src/app/mobile/mobile-casa-di-vito/mobile-casa-di-vito.component.ts
@@ -1,17 +1,32 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, ViewChild } from '@angular/core';
 import { Router } from '@angular/router';
+import { NgbCarousel, NgbCarouselModule } from '@ng-bootstrap/ng-bootstrap';
 import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-mobile-casa-di-vito',
   standalone: true,
-  imports: [ TranslateModule ],
+  imports: [ CommonModule, TranslateModule, NgbCarouselModule ],
   templateUrl: './mobile-casa-di-vito.component.html',
   styleUrl: './mobile-casa-di-vito.component.scss'
 })
 export class MobileCasaDiVitoComponent {
 
-  constructor(private router: Router) {}
+  @ViewChild('carousel', { static: true }) carousel!: NgbCarousel;
+
+  images = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13].map((n) => `assets/pictures/mobile/la casa di Cofano/${n}.jpg`);
+  isFullScreen: boolean = false;
+  
+  constructor() {}
+
+  toggleFullScreen() {
+    this.isFullScreen = !this.isFullScreen;
+    if (this.isFullScreen)
+      this.carousel.pause();
+    else
+      this.carousel.cycle();
+  }
 
   redirectTo(url: string) {
     window.location.href = url
